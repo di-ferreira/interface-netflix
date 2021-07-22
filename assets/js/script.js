85ac462261b612c6f868699f46d657aca0e89c4e
Diego Ferreira <diferreira.program@gmail.com>
2021-07-22 14:44:59 -0300

    Footer

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+let ActionContainer = document.querySelector("#action #carroussel");
+let ActionLeftArrow = document.querySelector("#action .netflix_content_leftArrow");
+let ActionRighttArrow = document.querySelector("#action .netflix_content_rightArrow");
+
+function rotateCarroussel(container, leftArrow, rightArrow) {
+    console.log("Container", container);
+    console.log("Left Arrow", leftArrow);
+    console.log("Right Arrow", rightArrow);
+}
+
+
+rotateCarroussel(ActionContainer, ActionLeftArrow, ActionRighttArrow);
